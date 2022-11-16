2c73048c5cf9c339bcee684253493c242c799898
danielbruno18 <danielbrun18@gmail.com>
2022-11-16 19:49:59 -0400

    first commit

---
+++ src/entrada_de_dados/TestandoWhile.java
@@ -0,0 +1,15 @@
+package entrada_de_dados;
+
+public class TestandoWhile {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		while (contador<10)
+	}
+	
+	
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Aluno
+ *
+ */
+module entrada_de_dados {
+}
